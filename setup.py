5fa3fc407c3961fc6823380ba585a454eb2a4fc1
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-12-17 19:58:15 +0900

    version 1.3.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/SuPar-Kanbun"
 
 setuptools.setup(
   name="suparkanbun",
-  version="1.3.7",
+  version="1.3.8",
   description="Tokenizer POS-tagger and Dependency-parser for Classical Chinese",
   long_description=long_description,
   long_description_content_type="text/markdown",
